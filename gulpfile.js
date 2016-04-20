03c457b429d2229ec8850e38f29f3abe47ed82cb
Levi <levimullan@gmail.com>
2016-04-19 22:57:27 -0400

    Gulp Functions working

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,26 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var pug = require('gulp-pug');
 
-gulp.task('default', function(){
+// POJOS, plain old javscript objects
+// task, src, dest, watch, pipe
 
-	// POJOS, plain old javscript objects
-	// task, src, dest, watch, pipe
+gulp.task('compileSass', function() {
+	gulp.src('./trendingColours.scss')
+		.pipe(sass({
+			outputStyle: 'compressed',
+		}))
+		.pipe(gulp.dest('./dest'))
+});
 
-	gulp.task('compileSass', function() {
-		gulp.src('./trendingColours.scss')
-			.pipe(sass({
-				outputStyle: 'compressed',
-			}))
-			.pipe(gulp.dest('./dest'))
-	});
+gulp.task('compilePug', function(){
+	gulp.src('./index.pug')
+		.pipe(pug())
+		.pipe(gulp.dest('./dest'))
+});
 
-	gulp.task('compilePug', function(){
-		gulp.src('./index.pug')
-			.pipe(pug({pretty: true}))
-			.pipe(gulp.dest('./dest'))
-	});
+gulp.task('watch' , function(){
+	gulp.watch('./trendingColours.scss', ['compileSass'])
+	gulp.watch('./index.pug',['compilePug'])
+});
 
-	gulp.task('watchCss', function(){
-		gulp.watch('trendingColours.scss', ['compileSass'])
-	});
 
-	gulp.task('watchHtml', function(){
-		gulp.watch('index.pug', ['compilePug'])
-	});
-
-})	
