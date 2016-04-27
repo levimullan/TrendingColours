00837bcbff5ee442fbd3a87b8a532894bd024045
Levi <levimullan@gmail.com>
2016-04-27 17:29:18 -0400

    added ractive

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
 import lemmons from './dribbble.ts'
 require('./styles/main.scss');
+var compile = require('./hexTrends.jade');
 declare var require: any;
+declare var Ractive: any;
 
-console.log(lemmons)
+console.log(compile());
 
+var lastname = "timmons";
+
+var ractive = new Ractive({
+	// The `el` option can be a node, an ID, or a CSS selector.
+	el: '#container',
+
+	// We could pass in a string, but for the sake of convenience
+	// we're passing the ID of the <script> tag above.
+	template: compile(),
+
+	// Here, we're passing in some initial data
+	data: { name: 'levi' }
+});
+
+setTimeout(function() {
+	ractive.set({ name: 'levi' });
+}, 3000);
