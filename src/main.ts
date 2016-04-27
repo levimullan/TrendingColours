7af6174c9a87346409747331f7fdbef47525228a
Levi <levimullan@gmail.com>
2016-04-27 19:46:12 -0400

    moved nav into new jade file

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import lemmons from './dribbble.ts'
 require('./styles/main.scss');
 var compile = require('./hexTrends.jade');
+
 declare var require: any;
 declare var Ractive: any;
 
-console.log(compile());
-
-var lastname = "timmons";
 
 var ractive = new Ractive({
 	// The `el` option can be a node, an ID, or a CSS selector.
-	el: '#container',
+	el: '#nav',
 
 	// We could pass in a string, but for the sake of convenience
 	// we're passing the ID of the <script> tag above.
@@ -20,6 +18,3 @@ var ractive = new Ractive({
 	data: { name: 'levi' }
 });
 
-setTimeout(function() {
-	ractive.set({ name: 'levi' });
-}, 3000);
